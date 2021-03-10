378a8e3dfc79197986556021f2cf92b06f841a53
Trancvundo <jaaovdpoel@gmail.com>
2021-03-10 16:14:07 +0100

    Add file

---
+++ CuringChamber.cpp
@@ -0,0 +1,382 @@
+
+//Libraries
+#include <DHT.h>
+#define UINT32_MAX uint32_t(-1)
+
+/*
+ If you change pins, change in constants
+ 
+// 8  - DHT22 (input pin)
+// 9  - Refrigerator
+// 10 - Heater
+// 11 - Humidifier
+// 12 - Dehumidifier
+ */
+
+//Select Tempature and Humidity
+int Chamber = 1; //Chamber number
+
+float Temp_Target = 14.0;
+float Temp_LT = 1.0;       // Low Tolerance (-)
+float Temp_HT = 1.5;       // High Tolerance (+)
+
+float Humi_Target = 60.0;
+float Humi_LT = 5.0;       // Low Tolerance (-)
+float Humi_HT = 7.5;       // High Tolerance (+)
+
+uint32_t MAX_SWITCH_RATE = 30; // Seconds (five minutes)
+
+
+
+
+//Constants
+#define DHTPIN 8     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+int r_Frid = 9;
+int r_Heat = 10;
+int r_Humi = 11;
+int r_Fan = 12;
+
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+uint32_t LastStateChangeTime;
+uint32_t LastCheckTime;
+unsigned long NumWrapArounds; // How many times has the clock wrapped
+unsigned long RuntimeInSeconds;
+
+enum State {
+  NOT_C,
+  C_UP,
+  C_DOWN
+};
+
+
+//Dict van controlledvalue, zorgt dat maar een void hoeft te maken 
+
+struct ControlledValue {
+  float Current;
+  float Target;
+  float Low_Tolerance;
+  float High_Tolerance;
+  int PinToIncrease;
+  int PinToDecrease;
+  int PinHigh; //switch 0-1 when relay is inverted
+  int PinLow;
+  uint32_t LastStateChangeTime; // In **seconds**, not milliseconds!
+  State state;
+};
+
+ControlledValue Temperature = {0, Temp_Target, Temp_LT, Temp_HT, r_Heat, r_Frid, 0,1,0, NOT_C };
+ControlledValue Humidity = {0, Humi_Target, Humi_LT,Humi_HT, r_Humi, r_Fan,1,0, 0, NOT_C };
+
+
+unsigned long LastAverageUpdateSeconds;
+unsigned long HourOfLastAverageUpdate;
+unsigned long DayOfLastAverageUpdate;
+float TempsLastHour[60];
+float TempsLastDay[24];
+float TempsLastMonth[30];
+float HumiditiesLastHour[60];
+float HumiditiesLastDay[24];
+float HumiditiesLastMonth[30];
+float AverageTempLastHour;
+float AverageTempLastDay;
+float AverageTempLastMonth;
+float AverageHumidityLastHour;
+float AverageHumidityLastDay;
+float AverageHumidityLastMonth;
+float AverageCoolingOvershoot;
+
+// waaarom moet dit?
+void updateState (ControlledValue &variable);
+void updateAverages (float temp, float hum);
+void getCurrentRuntime (int &days, int &hours, int &minutes, int &seconds);
+void printTime ();
+
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  NumWrapArounds = 0;
+  RuntimeInSeconds = 0;
+  LastCheckTime = 0;
+  
+  // This is needed when relay is inverted
+  pinMode(r_Frid,OUTPUT);
+  pinMode(r_Heat,OUTPUT);
+  pinMode(r_Humi,OUTPUT);
+  pinMode(r_Fan,OUTPUT);  
+  digitalWrite(r_Frid, Temperature.PinLow);
+  digitalWrite(r_Heat, Temperature.PinLow);
+  digitalWrite(r_Humi, Humidity.PinLow);
+  digitalWrite(r_Fan, Humidity.PinLow);
+
+  
+    for (int i = 0; i < 60; ++i) {
+      TempsLastHour[i] = 0;
+      HumiditiesLastHour[i] = 0;
+    }
+    for (int i = 0; i < 24; ++i) {
+      TempsLastDay[i] = 0;
+      HumiditiesLastDay[i] = 0;
+    }
+    for (int i = 0; i < 30; ++i) {
+      TempsLastMonth[i] = 0;
+      HumiditiesLastMonth[i] = 0;
+    }
+  LastAverageUpdateSeconds = 0;
+  HourOfLastAverageUpdate = 10000; // Make sure it hits the first time by initializing to nonsense
+  DayOfLastAverageUpdate = 10000; // Make sure it hits the first time by initializing to nonsense
+  
+  
+  AverageTempLastHour = 0;
+  AverageTempLastDay = 0;
+  AverageTempLastMonth = 0;
+  AverageHumidityLastHour = 0;
+  AverageHumidityLastDay = 0;
+  AverageHumidityLastMonth = 0;
+  AverageCoolingOvershoot = 0.0;
+  Serial.println ("\n\tDay\tHour\tMin\tSec\tT\tRH\tTemp\tHum.\tRunning Averages");//Need to change
+
+}
+
+void loop()
+{
+  uint32_t currentTime = millis();
+  if (currentTime < LastCheckTime) {
+    // Happens once every 50 days or so
+    NumWrapArounds++;
+  } 
+    LastCheckTime = currentTime;
+    RuntimeInSeconds = NumWrapArounds * (UINT32_MAX/1000) + (currentTime/1000);
+    const uint32_t pollRate = 15000; // Defaults to every fifteen seconds in the normal case
+  
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    
+    Temperature.Current = temp;
+    Humidity.Current = hum;
+    
+    
+    
+    
+    //Print temp and humidity values to serial monitor
+    printTime();
+    Serial.print (Temperature.Current,1);
+    Serial.print ("\t");
+    Serial.print (Humidity.Current,1);
+    Serial.print ("\t");
+    if (Temperature.state == C_UP) {
+        Serial.print("C_UP");
+    } else if (Temperature.state == C_DOWN) {
+        Serial.print("C_DOWN");
+    } else {
+        Serial.print("NOT_C");
+    }
+    Serial.print ("\t");
+    if (Humidity.state == C_UP) {
+        Serial.print("C_UP");
+    } else if (Humidity.state == C_DOWN) {
+        Serial.print("C_DOWN");
+    } else {
+        Serial.print("NOT_C");
+    }    
+    Serial.print ("\t");
+
+    updateState (Temperature);
+    updateState (Humidity);
+    updateAverages (Temperature.Current, Humidity.Current);  
+    
+    // For now, print the averages every time (when they are valid):
+    if (AverageTempLastHour > 0) {
+      Serial.print ("Hourly: ");
+      Serial.print (AverageTempLastHour);
+      Serial.print ("°C ");
+      Serial.print (AverageHumidityLastHour);
+      Serial.print ("% ");
+      if (AverageTempLastDay > 0) {
+        Serial.print (", Daily: ");
+        Serial.print (AverageTempLastDay);
+        Serial.print ("°C ");
+        Serial.print (AverageHumidityLastDay);
+        Serial.print ("% ");
+        if (AverageTempLastMonth > 0) {
+          Serial.print (", Monthly: ");
+          Serial.print (AverageTempLastMonth);
+          Serial.print ("°C ");
+          Serial.print (AverageHumidityLastMonth);
+          Serial.print ("% ");
+        }
+      }
+    }
+
+    Serial.println();
+
+    delay(pollRate); //Delay 15 sec.
+}
+// An utterly naive bang-bang controller with a timeout to prevent too-frequent activations 
+void updateState (ControlledValue &variable)
+{
+  switch (variable.state) {
+    case NOT_C:
+      if (RuntimeInSeconds > variable.LastStateChangeTime+MAX_SWITCH_RATE) {
+        if (variable.Current > variable.Target + variable.High_Tolerance) {
+          variable.LastStateChangeTime = RuntimeInSeconds;
+          variable.state = C_DOWN;
+          digitalWrite (variable.PinToDecrease, variable.PinHigh);
+        } else if (variable.Current < variable.Target - variable.Low_Tolerance) {
+          variable.LastStateChangeTime = RuntimeInSeconds;
+          variable.state = C_UP;
+          digitalWrite (variable.PinToIncrease, variable.PinHigh);
+        }
+      }
+      break;
+    case C_UP:
+      if (variable.Current > variable.Target) {
+        variable.LastStateChangeTime = RuntimeInSeconds;
+        variable.state = NOT_C;
+        digitalWrite (variable.PinToIncrease, variable.PinLow);
+      }
+      break;
+    case C_DOWN:
+      if (variable.Current < variable.Target) {
+        variable.LastStateChangeTime = RuntimeInSeconds;
+        variable.state = NOT_C;
+        digitalWrite (variable.PinToDecrease, variable.PinLow);
+      }
+      break;
+  }
+}
+
+void getCurrentRuntime (unsigned long &days, unsigned long &hours, unsigned long &minutes, unsigned long &seconds)
+{
+  const unsigned long secondsPerMinute = 60;
+  const unsigned long secondsPerHour = 3600;
+  const unsigned long secondsPerDay = 86400;
+  
+  unsigned long computationSeconds = RuntimeInSeconds;
+  days = computationSeconds / secondsPerDay;
+  computationSeconds = computationSeconds % secondsPerDay;
+  hours = computationSeconds / secondsPerHour;
+  computationSeconds = computationSeconds % secondsPerHour;
+  minutes = computationSeconds / secondsPerMinute;
+  computationSeconds = computationSeconds % secondsPerMinute;
+  seconds = computationSeconds;
+}
+
+
+void printTime ()
+{
+  unsigned long days, hours, minutes, seconds;
+  getCurrentRuntime (days, hours, minutes, seconds);
+  Serial.print (RuntimeInSeconds);
+  Serial.print ("\t");
+  Serial.print (days);
+  Serial.print ("\t");
+  Serial.print (hours);
+  Serial.print ("\t");
+  Serial.print (minutes);
+  Serial.print ("\t");
+  Serial.print (seconds);
+  Serial.print ("\t");
+}
+
+
+
+
+void updateAverages (float temp, float hum)
+{
+  unsigned long days, hours, minutes, seconds;
+  getCurrentRuntime (days, hours, minutes, seconds);
+  if (RuntimeInSeconds - LastAverageUpdateSeconds  >= 60) {
+    
+    // We just assume it's been about a minute (our usual polling rate)
+    LastAverageUpdateSeconds = RuntimeInSeconds; 
+    
+    // We are just going to do the easiest (and slowest) method here, shifting ALL the data, instead
+    // of keeping a pointer to the oldest.
+    float humidityIntegrator = 0.0;
+    float tempIntegrator = 0.0;
+    unsigned int count = 1;
+    for (int i = 0; i < 59 /*The last slot gets the current data*/; ++i) {
+      if (TempsLastHour[i+1] > 0.0) { // Has it been set yet?
+        count++;
+        TempsLastHour[i] = TempsLastHour[i+1];
+        tempIntegrator += TempsLastHour[i];
+        HumiditiesLastHour[i] = HumiditiesLastHour[i+1];
+        humidityIntegrator += HumiditiesLastHour[i];
+      }
+    }
+    TempsLastHour[59] = temp;
+    tempIntegrator += TempsLastHour[59];
+    HumiditiesLastHour[59] = hum;
+    humidityIntegrator += HumiditiesLastHour[59];
+    
+    AverageTempLastHour = tempIntegrator / count;
+    AverageHumidityLastHour = humidityIntegrator / count;
+    
+    if (minutes == 0 && HourOfLastAverageUpdate != hours) {
+      HourOfLastAverageUpdate = hours; // Make sure it only happens once per hour
+    
+      tempIntegrator = 0;
+      humidityIntegrator = 0;  
+      // Do the daily average the first minute of every hour:
+      count = 1;
+      for (int i = 0; i < 23; ++i) {
+        if (TempsLastDay[i+1] > 0.0) { // Has it been set yet?
+          count++;
+          TempsLastDay[i] = TempsLastDay[i+1];
+          tempIntegrator += TempsLastDay[i];
+          HumiditiesLastDay[i] = HumiditiesLastDay[i+1];  
+          humidityIntegrator += HumiditiesLastDay[i];
+        }
+      }
+      TempsLastDay[23] = AverageTempLastHour;
+      tempIntegrator += TempsLastDay[23];
+      HumiditiesLastDay[23] = AverageHumidityLastHour;
+      humidityIntegrator += HumiditiesLastDay[23];
+        
+      AverageTempLastDay = tempIntegrator / count;
+      AverageHumidityLastDay = humidityIntegrator / count;
+        
+      if (hours == 0 && DayOfLastAverageUpdate != days) {
+        DayOfLastAverageUpdate = days; // Make sure it only happens once per day
+        tempIntegrator = 0;
+        humidityIntegrator = 0;
+        count = 1;
+        for (int i = 0; i < 29; ++i) {
+          if (TempsLastMonth[i+1] > 0.0) {
+            count++;
+            TempsLastMonth[i] = TempsLastMonth[i+1];
+            tempIntegrator += TempsLastMonth[i];
+            HumiditiesLastMonth[i] = HumiditiesLastMonth[i+1];  
+            humidityIntegrator += HumiditiesLastMonth[i];
+          }
+        }
+        TempsLastMonth[29] = AverageTempLastDay;
+        tempIntegrator += TempsLastMonth[29];
+        HumiditiesLastMonth[29] = AverageHumidityLastDay;
+        humidityIntegrator += HumiditiesLastMonth[29];
+            
+        AverageTempLastMonth = tempIntegrator / count;
+        AverageHumidityLastMonth = humidityIntegrator / count;
+        
+        // Update the overshoot based on the average temp for the last day
+        float tOvershoot = Temp_Target - AverageTempLastDay;
+        // Adjust the overshoot correction value:
+        AverageCoolingOvershoot += tOvershoot;
+        Serial.print (" Overshoot updated to ");
+        Serial.print (AverageCoolingOvershoot);
+        Serial.write (176); // The degree symbol
+        Serial.print ("C. ");
+      }
+    }
+  }
+}   
